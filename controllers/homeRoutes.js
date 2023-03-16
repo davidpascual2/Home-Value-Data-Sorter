05ed2d8ea5a0127cedbcda67bcc6c77fe9160f48
David Pascual <davidpascual@Davids-MacBook-Pro.local>
2023-03-16 12:26:50 -0700

    sorted by price

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
     });
 });
 
-//==================================================
+//====================== STATE ROUTE ============================//
 
 router.get('/state' , (req, res) => {
     pool.getConnection((err, connection) => {
@@ -48,7 +48,7 @@ router.get('/state' , (req, res) => {
 })
 
 
-//==================================================
+//============ STATE FUNCTION ============//
 
 const groupByState = (data) => {
     const groupedData = {};
@@ -72,12 +72,60 @@ const groupByState = (data) => {
     });
 
     return sortedData;
-}
+};
 
-//==================================================
+
+
+//===================== PRICE ROUTE =====================//
 
 router.get('/price' , (req, res) => {
-    pool.getConnection((err, connection))
-})
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error connecting to database: '+ err.stack);
+            res.status(500).send('Internal server error'); //for user
+            return;
+        }
+
+        connection.query('SELECT * FROM mytable ORDER BY home_value', (error, results, fields) => {
+            connection.release();
+
+            if(error) {
+                console.error('Error retrieving data from database: '+ error.stack);
+                res.status(500).send('Internal server Error');
+                return;
+            }
+
+            var data = groupByPrice(results);
+            res.render('price', { data });
+        });
+    });
+});
+
+//=============== PRICE FUNCTION =================//
+
+const groupByPrice = (data) => {
+    const groupedData = {};
+
+    if(!Array.isArray(data)) {
+        return groupedData;
+    }
+
+    data.forEach(row => {
+        const price = row.price;
+
+        if(!groupedData[price]){
+            groupedData[price] = [];
+        }
+
+        groupedData[price].push(row);
+    });
+
+    const sortedData = {};
+    Object.keys(groupedData).sort().forEach(price => {
+        sortedData[price] = groupedData[price];
+    });
+    return sortedData
+};
+
 
 module.exports = router;
